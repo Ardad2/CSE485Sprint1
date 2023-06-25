df4f3c6f77ecda6056e47f31b175c362294885a3
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-24 21:22:09 -0700

    styled the list

--- screens/HomeScreen/BehaviorListScreen.js
+++ screens/HomeScreen/BehaviorListScreen.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, Modal, Image, SectionList} from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button, Modal, Image, SectionList, StatusBar} from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import { useSelector, dispatch , useDispatch} from 'react-redux';
 import { addBehavior, removeBehavior } from '../../store/redux/behaviors';
@@ -29,33 +29,39 @@ export default function BehaviorListScreen( {route, navigation} )
 {
     return (
       <View style={styles.container_style} > 
-        <Text style={styles.header} > Section List Exmaple </Text>
-        <SectionList sections={data} renderItem = { ({item}) => <Text> {item}</Text>}
-        renderSectionHeader={({section})  => <Text> {section.title} </Text>} 
+             
+
+        <SectionList sections={data} renderItem = { ({item}) => <Text style={styles.item_style}> {item}</Text>}
+        renderSectionHeader={({section})  => <Text style={styles.section_style}> {section.title} </Text>} 
         keyExtractor={(item, index) => index} />
 
         </View>
-    )
+            )
 };
 
 const styles = StyleSheet.create({
   container_style: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
   },
   item_style: {
-    backgroundColor: '#B591FF',
+    backgroundColor: 'white',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
     
   },header: {
-    textAlign: 'center',
-    backgroundColor: '#B2C2D2',
+    //textAlign: 'center',
+    backgroundColor: 'white',
     padding: 20,
     fontSize: 20,
    
+  },
+  section_style: {
+    //textAlign: 'center',
+    backgroundColor: 'white',
+    padding: 20,
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 });
-
   
